01d8f4d20398e0b5d936e6eb8f364fef71dfa113
kpoudyal06 <117843942+kpoudyal06@users.noreply.github.com>
2024-02-20 22:53:37 -0500

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,6 @@
+public class ArraySum {
+    public int sumOfArray(Integer[] a, int index) {
+        if (index == 0) return a[0];
+        else return a[index] + sumOfArray(a, index - 1);
+    }
+}
